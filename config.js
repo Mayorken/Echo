ade707d26e63e1cf795392714f590c6a631293be
Mayorken <kennethumoekpe@gmail.com>
2026-07-28 16:27:47 +0100

    Configure Privy Google login client

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Never place PRIVATE_KEY, STRIPE_SECRET_KEY, or any server secret here.
 window.ECHO_CONFIG = {
   apiUrl: 'http://localhost:3000',
-  privyAppId: '',
-  privyClientId: '',
+  privyAppId: 'cms4t034200qd0cjrsc4pbgam',
+  privyClientId: 'client-WY6c26d7EAoQGzbaBaCgbBG9hqpH3jbbBu6kW8pN3oonB',
 };
